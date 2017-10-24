dd57e10d16bedc51bf9407f9362a12f17967b6fb
Joyce <joycelee11@users.noreply.github.com>
2017-10-24 11:40:53 +0800

    update week1 hw

--- assignments/Joyce/week1-hw-joyce.js
+++ assignments/Joyce/week1-hw-joyce.js
@@ -2,9 +2,23 @@ let costs = [20, 45, 33, 41, 55, 92]
 
 // Question 1: try to get the sum of costs using for loop,
 // and assign the result to variable `sum`
-// https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Statements/for
+
+let sum = 0
+
+for (let i = 0; i < costs.length; i++) {
+	sum += costs[i]
+}
+
+console.log(sum)
 
 
 // Question 2: try to add 10% of cost to each elements in `costs`
 // and assign the results to variable `cost_with_fee`
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+
+let cost_with_fee = []
+
+for (let i = 0; i < costs.length; i++) {
+	cost_with_fee.push(costs[i] * 1.1)
+}
+
+console.log(cost_with_fee)
