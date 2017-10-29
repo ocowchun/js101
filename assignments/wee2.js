c854ec8fb925624f04b804d3139da97d68634b8c
ocowchun <ocowchun@gmail.com>
2017-10-29 22:38:38 +0800

    add assignment2

---
+++ assignments/wee2.js
@@ -0,0 +1,10 @@
+let numbers = [20, 45, 33, 41, 55, 92, 60, 71]
+
+// Question 1: given variable `numbers`, try to get all numbers that is divisible by 2
+// and assign the result to variable `answer1`
+
+// Question 2: given variable `numbers`, try to get all numbers that is divisible by 5 and less than 60
+// and assign the result to variable `answer2`
+
+// Question 3: find all prime number less than 100
+// and assign the result to variable `answer3`
