6baf2c4b1ea846e06af6782cc2351333e54d3042
ocowchun <ocowchun@gmail.com>
2017-10-31 21:36:12 +0800

    add assignment3

---
+++ assignments/week3.js
@@ -0,0 +1,13 @@
+// Question 1: use for loop to manipulate a tree like below:
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+//      *
+
+// Question 2: given a string str1 = ` how do  you turn this    on`
+// format it to `How do you turn this on` and assgn the result the variable `answer2`
+let str1 = ' how do  you turn this    on '
+
