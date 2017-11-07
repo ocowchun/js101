0553b57ee88b837264b6dc3a85d79b7b3d5544b1
Joyce <joycelee11@users.noreply.github.com>
2017-11-07 14:38:04 +0800

    update week4 notes

--- week4.js
+++ week4.js
@@ -1,4 +1,4 @@
-// common Math methods
+// Common Math methods
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 // sqrt
@@ -16,10 +16,14 @@ console.log(num2)
 let num3 = Math.random()
 console.log(num3)
 
+
 // round
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
-let num4 = Math.round(3.6)
+let num4 = Math.round(3.6) //4
 console.log(num4)
+
+let num4 = Math.floor(3.6) //3
+let num4 = Math.ceiling(3.6) //4
 
 // log
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/log
@@ -27,19 +31,30 @@ let num5 = Math.log(2)
 console.log(num5)
 
 
+// Functions
 // https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/Function
-function greeting(name) {
-  console.log(`Hi ${name}`)
+function greeting(greetingWord, name) { //function name(variable)
+// function greeting(greetingWord, name = 'Joyce') { //ES6 syntax
+	if (name === undefined){
+		name = 'there'
+	}
+	console.log(greetingWord + ' ' + name)
+  // console.log(`Hi ${name}`)
 }
 
-greeting('ben')
+greeting('Hi', 'Ben')
+greeting('Hello', 'Freda')
+greeting('Yo') //will print 'Yo undefined'
+
 
 function sum(a, b) {
-  return a + b
+  return a + b //回傳 value + ends function, should only appear ONCE in each function
+  console.log('hihihi') // won't execute
 }
 
 let s = sum(1, 2)
 console.log(s)
+
 
 let foo = function(){
   return 'bar'
@@ -47,13 +62,17 @@ let foo = function(){
 
 console.log(foo())
 
+
+//ES6
 function hello(name = 'ben'){
   console.log(`Hello ${name}`)
 }
 
 hello()
 
-function sumAndSquare(a, b){
+
+//function within a function
+function sumAndSquare(a, b){ //lower camelCase
   let s = sum(a, b)
   return Math.pow(s, 2)
 }
