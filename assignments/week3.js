fdff378c8f6da2adc07951e77ebe4d6436a853ce
ocowchun <ocowchun@gmail.com>
2017-11-07 23:02:22 +0800

    add assignment3

--- assignments/week3.js
+++ assignments/week3.js
@@ -6,8 +6,48 @@
 //  *********
 // ***********
 //      *
+let star = '*'
+let height = 7
+for (let i = 0; i < height; i++) {
+  let layer = star
+  for (let j = i; j < height - 1; j++) {
+    layer = ' ' + layer
+  }
+
+  if (i !== height - 1) {
+    for (let j = 0; j < i; j++) {
+      layer = layer + star + star
+    }
+  }else{
+    for (let j = 0; j < height - 1; j++) {
+      layer = ' ' + layer
+    }
+  }
+
+  console.log(layer)
+}
+
 
 // Question 2: given a string str1 = ` how do  you turn this    on`
 // format it to `How do you turn this on` and assgn the result the variable `answer2`
 let str1 = ' how do  you turn this    on '
 
+let a = str1.split(' ')
+console.log(a)
+
+// let answer2 = ''
+let strs = []
+for (let i = 0, max = a.length; i < max; i++) {
+  let word = a[i]
+  if (word !== '') {
+    if (strs.length === 0) {
+      let wordLength = word.length
+      let newWord = word.toUpperCase()[0] + word.slice(1, wordLength)
+      strs.push(newWord)
+    } else {
+      strs.push(word)
+    }
+  }
+}
+
+console.log(strs.join(' ') === 'How do you turn this on')
